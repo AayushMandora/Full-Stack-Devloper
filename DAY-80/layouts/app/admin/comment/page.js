5c7fdb0b3a94585948ffee0eaa4a5df30faba0fe
AayushMandora <aayushmandora76@gmail.com>
2024-04-22 21:04:21 +0530

    Layouts in Next.js

---
+++ DAY-80/layouts/app/admin/comment/page.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const comment = () => {
+  return (
+    <div>This is Admin comment</div>
+  )
+}
+
+export default comment
---
+++ DAY-80/layouts/app/admin/login/page.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const login = () => {
+  return (
+    <div>This is Admin login</div>
+  )
+}
+
+export default login
